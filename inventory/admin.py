91b28254e994b624f29c450c6d6bc5ea3fcf5039
NguyenHo12 <nho4@ivytech.edu>
2025-05-02 11:13:36 +0700

    fix UI issus

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,14 +1,24 @@
-
-
 from django.contrib import admin
-from .models import Supply
+from .models import Supply, Category, Tag
 
 class SupplyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'quantity', 'location')  
-    search_fields = ('name',)  
-    ordering = ('name',)  
+    list_display = ('name', 'price', 'quantity', 'location', 'category')  
+    search_fields = ('name', 'category__name')  
+    list_filter = ('category', 'tags')
+    ordering = ('name',)
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
 
 admin.site.register(Supply, SupplyAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
 
 
