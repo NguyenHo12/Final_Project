b906ba47f8b67593b349a72efa63aac1d5cfe28b
NguyenHo12 <nho4@ivytech.edu>
2025-05-05 20:19:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # Import the WSGI application from Django
-from inventory_project.wsgi import application
+from config.wsgi import application
 
 # This is the application object that gunicorn will use
 app = application 
